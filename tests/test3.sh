448be30c852cf68f73697f318b6794767b22a461
spagad7 <spagad7@github.com>
2018-04-25 18:35:39 -0500

    updated test3.sh

--- tests/test3.sh
+++ tests/test3.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
-
+dir=$(pwd)
 echo -e "\n\n******************************** test case 3 ********************************\n"
 
 echo -e "\tcreating producer and consumer processes..\n"
-./producer/producer 50 BLACK &
+./$dir/../producer/producer 50 BLACK &
 PID1=$!
-./consumer/consumer 100 A &
+./$dir/../consumer/consumer 100 A &
 PID2=$!
 wait "$PID1" "$PID2"
-
