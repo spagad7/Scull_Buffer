eea62f13c34b0d08787e2252127906e98f723329
spagad7 <spagad7@github.com>
2018-04-25 22:23:07 -0500

    updated tests

--- tests/test3/test3.sh
+++ tests/test3/test3.sh
@@ -3,8 +3,8 @@ cur_dir=$(pwd)
 echo -e "\n\n **************** Test 3 ****************"
 echo -e "\nCreating Producer: BLACK; n_items = 50"
 echo -e "\nCreating Consumer: C1; n_items = 100\n"
-$cur_dir/../producer/producer 50 BLACK &
+$cur_dir/../../producer/producer 50 BLACK &
 PID1=$!
-$cur_dir/../consumer/consumer 100 C1 &
+$cur_dir/../../consumer/consumer 100 C1 &
 PID2=$!
 wait "$PID1" "$PID2"
--- tests/test5/test5.sh
+++ tests/test5/test5.sh
@@ -4,10 +4,10 @@ echo -e "\n\n **************** Test 5 ****************"
 echo -e "\nCreating Producer: WHITE; n_items = 50"
 echo -e "\nCreating Consumer: C1; n_items = 25"
 echo -e "\nCreating Consumer: C2; n_items = 25\n"
-$cur_dir/../producer/producer 50 WHITE &
+$cur_dir/../../producer/producer 50 WHITE &
 PID1=$!
-$cur_dir/../consumer/consumer 25 C1 &
+$cur_dir/../../consumer/consumer 25 C1 &
 PID2=$!
-$cur_dir/../consumer/consumer 25 C2 &
+$cur_dir/../../consumer/consumer 25 C2 &
 PID3=$!
 wait "$PID1" "$PID2" "$PID3"
