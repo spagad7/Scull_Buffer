b084f8c62837e3812dcac30244d303ed236801c0
spagad7 <spagad7@github.com>
2018-04-25 21:33:57 -0500

    updated producer and consumer to wait

--- tests/test1.sh
+++ tests/test1.sh
@@ -5,7 +5,6 @@ echo -e "\n\n******************************** test case 1 **********************
 echo -e "\tcreating producer and consumer processes..\n"
 $cur_dir/../producer/producer 50 BLACK &
 PID1=$!
-sleep 2
 $cur_dir/../consumer/consumer 50 A &
 PID2=$!
 wait "$PID1" "$PID2"
